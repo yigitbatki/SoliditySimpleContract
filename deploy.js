d67080b60fa81ade5465c45fba891c388246e0f1
yigitbatki <batkiyigit@gmail.com>
2022-03-20 12:41:31 +0300

    SimpleSolidityContract
    A solidity contract with a function to access and change initial value. Mocha test structure is also implemented.

---
+++ deploy.js
@@ -0,0 +1,21 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {interface,bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+  'YOUR_MNEMONIC',
+  'YOUR_PROVIDER_URL'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('attempting to deploy from account', accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data:bytecode,arguments:['yeah boy']})
+    .send({gas:'1000000',from:accounts[0]});
+
+  console.log('contract deployed to:',result.options.address);
+  provider.engine.stop();
+};
+deploy();
